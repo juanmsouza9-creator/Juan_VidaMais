87969dbcbc47482d818de9ed39966b09cbce45db
Juan Macos de Souza <juan.m.souza9@aluno.senai.br>
2025-11-07 13:25:40 +0000

    Criação da função tese_calcular e test-classificar

---
+++ Vida+/test_imc.py
@@ -0,0 +1,11 @@
+from imc import *
+def  test_calcular_imc():
+    assert calcular_imc(70, 1.75) == 22.86
+
+def test_classificar_imc():
+    assert classificar_imc(17.9) == "Abaixo do peso"
+    assert classificar_imc(22.0) == "Peso normal"
+    assert classificar_imc(27.3) == "Sobrepeso"
+    assert classificar_imc(33.0) == "Obesidade grau I"
+    assert classificar_imc(37.0) == "Obesidade grau II"
+    assert classificar_imc(45.0) == "Obesidade grau III"
